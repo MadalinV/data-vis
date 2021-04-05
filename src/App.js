3f577f26603d1c3ca3b0a484e2e26b89d64bfea7
Madalin Cosmin <madalinvasile91@gmail.com>
2021-04-05 22:54:22 +0300

    data visualization update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component,Fragment } from "react";
+import React, {  Fragment } from "react";
 import mockData from "./mockData";
-import Histogram from './Histogram'
-console.log(mockData.data.allPosts);
+import Histogram from "./Histogram";
+
 
 const months = {
 	Jan: 1,
@@ -19,37 +19,44 @@ const months = {
 };
 
 let postsByMonth = {};
-		let posts = mockData.data.allPosts;
-		posts.forEach(post => {
-			let month = post.createdAt.split(" ")[1];
-			let year = post.createdAt.split(" ")[3];
-			if (year === "2019") {
-				if (!postsByMonth[month]) {
-					postsByMonth[month] = [post];
-				} else {
-					postsByMonth[month].push(post);
-				}
-			}
-		});
-		let postsByMonthSorted = Object.keys(postsByMonth)
-			.map(month => ({ month, posts: postsByMonth[month] }))
-			.sort((a, b) => months[a.month] - months[b.month]);
-      
-
-export default class App extends Component {
-
-	render() {
-		return (
-      <Fragment>
-          <div style={{width: '100%', minHeight: '100vh', display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
-            <h1>Number of Posts per month</h1>
-            <Histogram
-              data={postsByMonthSorted}
-              width={1000}
-              height={600}
-            />
-          </div>
-        </Fragment>
-    );
+let posts = mockData.data.allPosts;
+posts.forEach(post => {
+	let month = post.createdAt.split(" ")[1];
+	let year = post.createdAt.split(" ")[3];
+	if (year === "2019") {
+		if (!postsByMonth[month]) {
+			postsByMonth[month] = [post];
+		} else {
+			postsByMonth[month].push(post);
+		}
 	}
+});
+let postsByMonthSorted = Object.keys(postsByMonth)
+	.map(month => ({ month, posts: postsByMonth[month] }))
+	.sort((a, b) => months[a.month] - months[b.month]);
+
+export default function App() {
+	
+		return (
+			<Fragment>
+				<div
+					style={{
+						width: "100%",
+						minHeight: "100vh",
+						display: "flex",
+						justifyContent: "center",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<h1>Number of Posts per month</h1>
+					<Histogram
+						data={postsByMonthSorted}
+						width={1000}
+						height={600}
+					/>
+				</div>
+			</Fragment>
+		);
+	
 }
